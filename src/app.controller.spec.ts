c9d99b4f4215a20bff8dcc07bb0d1d7526f850ab
Burak Akkaya <mbrka055@gmail.com>
2023-03-07 20:19:37 +0300

    test:app.controller updated.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,7 +7,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -15,7 +15,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.checker()).toBe('pong');
     });
   });
 });
