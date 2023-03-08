24ea7840503718ec77a335826015a8f8e333e960
Burak Akkaya <mbrka055@gmail.com>
2023-03-08 18:26:18 +0300

    env updated

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  await app.listen(
+    +configService.get<string>('PORT'),
+    configService.get<string>('HOST'),
+  );
 }
 bootstrap();
